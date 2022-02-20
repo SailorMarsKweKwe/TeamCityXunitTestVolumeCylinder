561a7579139cebe03ead3418e18cbaa6924faa5f
SailorMarsKweKwe <98244600+SailorMarsKweKwe@users.noreply.github.com>
2022-02-20 16:29:58 +0200

    Update MethodClass.cs

--- TestProject2/MethodClass.cs
+++ TestProject2/MethodClass.cs
@@ -9,6 +9,7 @@ namespace TestProject2
             public static double VolumCylinder(double r, double h)
             {
                 return Math.PI * Math.Pow(r, 2) * h;
-            }       
+            }   
+        // Commits.
     }
 }
